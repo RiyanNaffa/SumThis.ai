7611be8490960dc0836304b8069114bb27ba9d45
IanRyan <riyannaffanusafara@gmail.com>
2025-05-25 20:15:56 +0700

    feat: change header

--- sumthis.ai/src/components/Header.jsx
+++ sumthis.ai/src/components/Header.jsx
@@ -1,19 +1,16 @@
 const Header = ({ title }) => {
     return (
-        <div className="flex justify-between items-center p-4 bg-blue-500 text-white">
-            <div className="flex items-center space-x-3">
-                <img src="/finfit.svg" alt="Logo" className="h-10 w-auto" style={{ maxHeight: '2.5rem' }} />
-                <h1 className="text-3xl font-bold">{title}</h1>
+        <div className="justify-between items-center p-4 bg-blue-500 text-white">
+            <div className="flex items-center space-x-4">
+                <div><a href="/"><img src="/finfit.svg" alt="Logo" className="h-20 w-auto" style={{ maxHeight: '4rem' }} /></a></div>
+                <div><a href="/"><h1 className="text-3xl font-bold">{title}</h1></a></div>
             </div>
             <div className="flex space-x-4">
-                <a href="/" className="text-white hover:text-gray-200">
-                    Home
-                </a>
                 <a href="/about" className="text-white hover:text-gray-200">
                     About
                 </a>
-                <a href="/contact" className="text-white hover:text-gray-200">
-                    Contact
+                <a href="/history" className="text-white hover:text-gray-200">
+                    History
                 </a>
             </div>
         </div>
