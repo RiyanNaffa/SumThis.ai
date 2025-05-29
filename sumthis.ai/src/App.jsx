47ee23b8f88c34efe7ba63c7fb39d98f72f42147
IanRyan <riyannaffanusafara@gmail.com>
2025-05-29 07:59:21 +0700

    feat: add routing, modify styling

--- sumthis.ai/src/App.jsx
+++ sumthis.ai/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
+import PdfPreview from "./components/pages/PdfPreview";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/pdf-preview" element={<PdfPreview />} />
       </Routes>
     </Router>
   );
